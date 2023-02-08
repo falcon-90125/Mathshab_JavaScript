2b7c4fb09d9d981f23883a465a3918e0be2aaafc
falcon-90125 <sokolov_aa@ngs.ru>
2023-02-08 19:43:02 +0700

    initial

---
+++ script.js
@@ -0,0 +1,32 @@
+// Браузерные команды:
+// 1.Модальное окно - Выводит какой либо сообщение для исполнения какого-либо действия
+// alert('Hello!')
+// 2. Модальное окно - Выводит какой либо сообщение для исполнения какого-либо действия с вариантом "Отмена"
+// confirm('Вы учите JS?')
+// 3. Модальное окно - Выводит какой либо сообщение для исполнения какого-либо действия с вариантом "Отмена" и ответом по умолчанию
+// prompt('Как вы учите JS?', 'Упорно!')
+
+// Задать переменную (let во многом схож с var, но в var есть отличия, которые делают его(var) не желательным)
+// let a = 5
+// Задать постоянную
+const skill = document.getElementById('skill')
+const isLove = document.getElementById('isLove')
+const string = document.getElementById('string')
+
+// Вывод элемента в консоль
+// console.log(span)
+// Вывод внутреннего текста элемента - вытаскивает "буквально всё" - весь HTML-код
+// console.log(span.innerHTML)
+
+// Вывод внутреннего текста элемента - вытаскивает HTML-тэги - только текст, котрый находится внутри элемента
+// console.log(span.innerText)
+
+// const skillText = prompt('Какой язык Вы учите?', 'ХЗ!')
+// const isLoveValue = confirm('Любите ли Вы этот язык???')
+
+// console.log(isLoveValue)
+// console.log(document.getElementById('example').innerHTML) 
+
+// skill.innerText = skillText
+// isLove.innerText = isLoveValue
+
